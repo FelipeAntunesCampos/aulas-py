71369c0197a48f5fc1d3dfb5398bffe86851ae19
FelipeCampos_ <felipe.a.campos7@aluno.senai.br>
2025-09-15 07:49:04 -0300

    Adiciona def calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -1,15 +1,16 @@
+def calculo():
+  print("Selecione a operação:")
+  print("1. Adição")
+  print("2. Subtração")
+  print("3. Multiplicação")
+  print("4. Divisão")
 
-
-print("Selecione a operação:")
-print("1. Adição")
-print("2. Subtração")
-print("3. Multiplicação")
-print("4. Divisão")
+calculo();
 
 escolha = input("Digite sua escolha (1/2/3/4): ")
 
-valor1 = float(input("Digite o valor desejado"))
-valor2 = float(input("Digite o segundo valor"))
+valor1 = float(input("Digite o valor desejado "))
+valor2 = float(input("Digite o segundo valor "))
 
 
 if escolha == '1':
@@ -25,4 +26,4 @@ elif escolha =='4':
     resultado = valor1 + valor2 
     print(resultado)
 else: 
-    print('Você deve selecionar alguma operação')
+    print('Você deve selecionar alguma operação') 
